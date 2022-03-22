4ada81e6fc173cf5681af63ce55f0e5a36174536
Francisco Rafart <francisco.rafart@gmail.com>
2022-03-22 11:29:40 -0400

    Start front end

--- index.mjs
+++ index.mjs
@@ -1,27 +1,32 @@
 import {loadStdlib} from '@reach-sh/stdlib';
 import * as backend from './build/index.main.mjs';
+import  {ask, yesno, done } from '@reach-sh/stdlib/ask.mjs';
+
 const stdlib = loadStdlib(process.env);
 
-  const startingBalance = stdlib.parseCurrency(100);
-
-  const [ accAlice, accBob ] =
-    await stdlib.newTestAccounts(2, startingBalance);
-  console.log('Hello, Alice and Bob!');
-
+(async () => {
+  console.log('Crowdfunding platform');
   console.log('Launching...');
-  const ctcAlice = accAlice.contract(backend);
-  const ctcBob = accBob.contract(backend, ctcAlice.getInfo());
 
-  console.log('Starting backends...');
-  await Promise.all([
-    backend.Alice(ctcAlice, {
-      ...stdlib.hasRandom,
-      // implement Alice's interact object here
-    }),
-    backend.Bob(ctcBob, {
-      ...stdlib.hasRandom,
-      // implement Bob's interact object here
-    }),
-  ]);
+  const isArtist = await ask('Are you using the app as an artist?', yesno);
+  const acc = stdlib.newTestAccount(stdlib.parseCurrency(isArtist? 10 : 100));
+  let ctc = null;
 
-  console.log('Goodbye, Alice and Bob!');
+  if (isArtist) {
+    ctc = acc.contract(backend);
+    ctc.getInfo().then(info => {
+      console.log(`The contract is at ${JSON.stringify(info)}`);
+    })
+  } else {
+    const info  = ask('Paste contract info', JSON.parse);
+    ctc = acc.contract(backend, info);
+  }
+
+  const ftm = x => stdlib.formatCurrency(x, 4);
+  const getBalance = async () => ftm(await stdlib.balanceOf(acc));
+
+  const interact = { ...stdlib.hadRandom };
+
+  // TODO: Define functions for participants
+
+});
